dc051dda99d7e39cd1cd95dc70cf69041ed930ee
LieStage <118051769+LieStage@users.noreply.github.com>
2023-06-29 09:23:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -69,8 +69,8 @@ AUTO_DELETE_SECONDS = int(environ.get('AUTO_DELETE_SECONDS', 120))
 
 FILE_REQ_CHANNEL = int(environ.get('FILE_REQ_CHANNEL', LOG_CHANNEL))
 
-SHORTNER_SITE =  environ.get('SHORTNER_SITE', '') #Put Only Shortner Site domain don't put like this https://tnlink.in/
+SHORTNER_SITE =  environ.get('SHORTNER_SITE', 'kpslink.in') #Put Only Shortner Site domain don't put like this https://tnlink.in/
 
-SHORTNER_API =  environ.get('SHORTNER_API', '')
+SHORTNER_API =  environ.get('SHORTNER_API', '4cd3ae536ddce7066195ae87dc70dd4c556dcb14')
 
 AUTO_DELETE =  environ.get('AUTO_DELETE', 'True')
